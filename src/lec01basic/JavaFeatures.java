aabf3263d29acec4f0f05807d4ecf228544a5d3f
webturing <zj@webturing.com>
2019-02-22 20:31:53 +0800

    第一讲提纲 基础算法和Java特性

---
+++ src/lec01basic/JavaFeatures.java
@@ -0,0 +1,15 @@
+package lec01basic;
+
+
+import java.text.SimpleDateFormat;
+
+public class JavaFeatures {
+
+    public static void main(String[] args) {
+        double pi = Math.PI;
+        System.out.println(pi);
+        System.out.printf("%.3f\n", pi);
+        System.out.println(String.format("%.3f", pi));
+
+    }
+}
