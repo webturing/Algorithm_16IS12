9ed8363f7fbf1ccd11f2404d1399ebdc1901ebbf
webturing <zj@webturing.com>
2019-03-03 22:28:12 +0800

    完善了Sqrt算法，

--- src/lec02algorithm/Sqrt.java
+++ src/lec02algorithm/Sqrt.java
@@ -12,12 +12,12 @@ public class Sqrt {
 
     static void solve(int n) {//O(sqrt(n))
         int m = 2 * n;
-        for (int p = 1; p * p <= n; p++) {
+        for (int p = (int) Math.sqrt(m); p >= 1; p--) {
             if (m % p != 0) continue;
             int q = m / p;
             int i = (q - p + 1) / 2;
             int j = (p + q - 1) / 2;
-            if (i + j == q && j - i + 1 == p) {
+            if (i + j == q && j - i + 1 == p && i != j) {
                 System.out.println(String.format("%d %d", i, j));
             }
         }
