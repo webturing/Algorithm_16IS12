bcd3d46d9617a319ccdf9ee186a491ec5b6b3dcf
webturing <zj@webturing.com>
2019-03-19 17:28:15 +0800

    第7讲 数据结构和Java集合框架

--- src/lec07ds/DoubleColoredBall.java
+++ src/lec07ds/DoubleColoredBall.java
@@ -5,25 +5,31 @@ import java.util.List;
 import java.util.Vector;
 
 public class DoubleColoredBall {
-	public static void main(String[] args) {
-		Vector<Integer> balls = new Vector<Integer>();
-		for (int i = 1; i <= 35; i++)
-			balls.add(i);
-		System.out.println(balls);
-		Collections.shuffle(balls);
-		System.out.println(balls);
-		Vector<Integer> firstPrize = new Vector<Integer>();
-		for (int i = 0; i < 7; i++)
-			firstPrize.add(balls.get(i));
-		System.out.println(firstPrize);
-		for (int i = 0; i < 10000000; i++) {
-			Collections.shuffle(balls);
-			List<Integer> user = balls.subList(0, 7);
-			// System.out.println(user);
-			if (user.equals(firstPrize)) {
-				System.out.println("5,000,000 RMB!");
-			}
+    public static void main(String[] args) {
 
-		}
-	}
+        Vector<Integer> balls = new Vector<>();
+        for (int i = 1; i <= 35; i++) {
+            balls.add(i);
+
+
+        }
+        System.out.println(balls);
+        Collections.shuffle(balls);
+        System.out.println(balls);
+        Vector<Integer> prize = new Vector<>();
+        for (int i = 0; i < 6; i++) {
+            prize.add(balls.get(i));
+        }
+        System.out.println(prize);
+        while (true) {
+            Collections.shuffle(balls);
+            List<Integer> a = balls.subList(0, 6);
+            if (prize.equals(a)) {
+                System.out.println("$5000000");
+                break;
+            }
+
+        }
+
+    }
 }
--- src/lec07ds/P1220a.java
+++ src/lec07ds/P1220a.java
@@ -4,21 +4,14 @@ import java.util.Arrays;
 
 public class P1220a {
 	public static void main(String[] args) {
-		int a[] = { 1, 1, 6, 3, 3 };
+		int a[] = {1, 1, 6, 3, 3};
 		Arrays.sort(a);
-		System.out.println(Arrays.toString(a));
-		int[] b = new int[a.length];
-		int j = 0;
-		b[0] = a[0];
-		j++;
+		System.out.println(a[0]);
+		int recent = a[0];
 		for (int i = 1; i < a.length; i++) {
-			if (a[i] == b[j - 1])
-				continue;
-			b[j] = a[i];
-			j++;
+			if (a[i] == recent) continue;
+			System.out.println(a[i]);
+			recent = a[i];
 		}
-		// System.out.println(Arrays.toString(b));
-		for (int i = 0; i < j; i++)
-			System.out.println(b[i]);
 	}
 }
