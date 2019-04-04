82c1dae5c92977a756c7cb3f59dad2e30d77e9a4
webturing <zj@webturing.com>
2019-04-04 15:01:39 +0800

    第12讲：期中试题讲解

--- src/lec12midtest/D.java
+++ src/lec12midtest/D.java
@@ -9,24 +9,24 @@ public class D {
 
     public static void main(String[] args) {
         Vector<String> missed = new Vector<>();
-        Vector<String> recieved = new Vector<>();
+        Vector<String> received = new Vector<>();
         Vector<String> dialed = new Vector<>();
         while (cin.hasNext()) {
             int type = cin.nextInt();
             String number = cin.next();
             if (type == 0) missed.add(number);
-            else if (type == 1) recieved.add(number);
+            else if (type == 1) received.add(number);
             else if (type == 2) dialed.add(number);
         }
         int max = 10;
         max = Math.max(missed.size(), max);
-        max = Math.max(recieved.size(), max);
+        max = Math.max(received.size(), max);
         max = Math.max(dialed.size(), max);
         while (missed.size() < max) missed.add("0");
-        while (recieved.size() < max) recieved.add("0");
+        while (received.size() < max) received.add("0");
         while (dialed.size() < max) dialed.add("0");
         for (int i = 0; i < max; i++) {
-            System.out.println(String.format("%s %s %s", missed.get(i), recieved.get(i), dialed.get(i)));
+            System.out.println(String.format("%s %s %s", missed.get(i), received.get(i), dialed.get(i)));
         }
     }
 }
