61dc49012566419afcbc84e83de1def2efe74a30
webturing <zj@webturing.com>
2019-04-11 12:51:04 +0800

    滚动方法扩展素数表

--- src/lec14dp2/D3.java
+++ src/lec14dp2/D3.java
@@ -15,19 +15,29 @@ public class D3 {
 
     public static void main(String[] args) {
         int n = 20;
+        fill(n);
+        int tot = 0;
+        for (int i = 0; i < primes.size() - 1 && primes.get(i) + 2 <= n; i++) {
+            if (primes.get(i) + 2 >= primes.get(i + 1)) ++tot;
+        }
+        System.out.println(tot);
+    }
+
+    private static void fill(int n) {
         if (primes.lastElement() < n) {
             for (int k = primes.lastElement() + 2; k <= n; k += 2) {
                 boolean flag = true;
-                for (Integer p : primes)
-                    if (p * p <= k && k % p == 0) {
+                for (Integer p : primes) {
+                    if (p * p > k) break;
+                    if (k % p == 0) {
                         flag = false;
                         break;
                     }
+                }
                 if (flag) {
                     primes.add(k);
                 }
             }
         }
-        System.out.println(primes);
     }
 }
