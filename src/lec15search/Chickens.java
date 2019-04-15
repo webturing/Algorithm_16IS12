ca40a62e6d08d8cf83ead5b14ba69837d6708c4c
webturing <zj@webturing.com>
2019-04-15 13:10:02 +0800

    搜索

--- src/lec15search/Chickens.java
+++ src/lec15search/Chickens.java
@@ -6,11 +6,11 @@ package lec15search;
  * 5x+3y+z/2=100
  */
 public class Chickens {
-    final static int N = 1000;
+    final static int N = 100;
 
     public static void main(String[] args) {//O(1/30*n^3)=>O(n^2)
         for (int x = 0; x <= N / 5; x++)
-            for (int y = 0; y <= N / 3; y++) {
+            for (int y = 0; y <= (N - 5 * x) / 3; y++) {
                 int z = N - x - y;
                 if (x + y + z == N && 5 * x + 3 * y + z / 2 == N && z % 2 == 0) {
                     System.out.println(String.format("%d %d %d", x, y, z));
